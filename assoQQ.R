7f7cedeacbdaa9f5163005bcab64c97db91ae5c5
Jen-Hsiang Ou <oumark.me@outlook.com>
2022-02-21 12:14:54 +0100

    Update assoQQ.R

--- assoQQ.R
+++ assoQQ.R
@@ -1,9 +1,6 @@
 library(tidyverse)
 library(compiler)
 
-library(da)
-gwrst = fread("./result/FWsel/all66.assoc.linear.gz")
-
 assoQQ = function(gwrst, path = NULL){
     p = ggplot(data = gwrst, aes(sample = P)) +
         stat_qq() +
