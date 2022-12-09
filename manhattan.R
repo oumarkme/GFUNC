e18bbd96292b94dfd2b793da5f901564deab0380
Jen-Hsiang Ou <oumark.me@outlook.com>
2022-12-09 15:17:10 +0100

    Update manhattan.R

--- manhattan.R
+++ manhattan.R
@@ -1,35 +1,52 @@
 library(tidyverse)
 library(compiler)
 
-manhattanPlot = function(asso, pointAlpha = 0.8, pointSize = 0.8, pointCol = c('grey', 'skyblue'), saveDir = NULL){
-    asso = asso %>% mutate(nlogFDR = -log10(p.adjust(.$P, method = 'fdr')))
-    don = asso %>%
-        group_by(CHR) %>%
-        summarise(chr_len=max(BP)) %>%
-        mutate(tot=cumsum(chr_len) - chr_len) %>%
-        select(-chr_len) %>%
-        left_join(asso, ., by=c("CHR"="CHR")) %>%
-        arrange(CHR, BP) %>%
-        mutate(BPcum = BP + tot)
-    axisdf = don %>%group_by(CHR) %>% summarise(center = (max(BPcum) + min(BPcum))/2)
+manhattanPlot = function(asso, saveDir = NULL, pAdjust = 'none', cutRatio = 0, pointAlpha = 0.8, pointSize = 0.8, figWidth = 15, figHeight = 4, pointCol = c('dimgrey', 'grey')){   
     
-    returnPlot = ggplot( don, aes(x = BPcum, y = nlogFDR) ) +
-        geom_point( aes(color=as.factor(CHR)), alpha = pointAlpha, size = pointSize ) +
-        scale_color_manual( values = rep(pointCol, length(unique(don$CHR))) ) +
-        scale_x_continuous( label=axisdf$CHR, breaks=axisdf$center ) +
-        scale_y_continuous( expand = c(0, 0), limits = c(0, round(max(asso$nlogFDR)*1.25)) ) +
-        theme_bw() +
-        theme(
-            legend.position='none',
-            panel.border = element_blank(),
-            panel.grid.major.x = element_blank(),
-            panel.grid.minor.x = element_blank()
-        )
+    asso = asso %>% mutate(nlogFDR = -log10(p.adjust(.$P, method = pAdjust))) %>% filter(.$nlogFDR >= quantile(.$nlogFDR, cutRatio))
     
-    if(is.null(saveDir)){
+    if(length(unique(asso$CHR)) > 1){
+        don = asso %>%
+            group_by(CHR) %>%
+            summarise(chr_len=max(BP)) %>%
+            mutate(tot=cumsum(chr_len) - chr_len) %>%
+            select(-chr_len) %>%
+            left_join(asso, ., by=c("CHR"="CHR")) %>%
+            arrange(CHR, BP) %>%
+            mutate(BPcum = BP + tot)
+        axisdf = don %>%group_by(CHR) %>% summarise(center = (max(BPcum) + min(BPcum))/2)
+    
+        returnPlot = ggplot( don, aes(x = BPcum, y = nlogFDR) ) +
+            geom_point( aes(color=as.factor(CHR)), alpha = pointAlpha, size = pointSize ) +
+            scale_color_manual( values = rep(pointCol, length(unique(don$CHR))) ) +
+            scale_x_continuous( label=axisdf$CHR, breaks=axisdf$center, name = 'Chromosome' ) +
+            scale_y_continuous( expand = c(0, 0), limits = c(min(asso$nlogFDR), round(max(asso$nlogFDR)*1.15)), name='-log10(q-value)') +
+            theme_bw() +
+            theme(
+                legend.position='none',
+                panel.border = element_blank(),
+                panel.grid.major.x = element_blank(),
+                panel.grid.minor.x = element_blank()
+            )
+    }else{
+        returnPlot = ggplot( asso, aes(x = BP/1e6, y = nlogFDR) ) +
+            geom_point( color = pointCol[1], alpha = pointAlpha, size = pointSize) +
+            scale_y_continuous( expand = c(0, 0), limits = c(min(asso$nlogFDR), round(max(asso$nlogFDR)*1.1)), name='-log10(q-value)') +
+            scale_x_continuous( name = 'Position (Mb)') +
+            theme_bw() +
+                theme(
+                    legend.position='none',
+                    panel.border = element_blank(),
+                    panel.grid.major.x = element_blank(),
+                    panel.grid.minor.x = element_blank()
+                )
+    }
+    
+    
+    if(is_null(saveDir)){
         return(returnPlot)
     }else{
-        ggsave(saveDir, returnPlot, width = 20, height = 4)
+        ggsave(saveDir, returnPlot, width = figWidth, height = figHeight, dpi = 300, limitsize = FALSE)
         cat(paste('Figure saved:', saveDir))
     }
 }
